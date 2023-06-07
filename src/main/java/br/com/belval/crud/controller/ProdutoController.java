42f85c937878f6e3d3eae2eb74ea4fb073640e1d
Paulo <rm89569@estudante.fieb.edu.br>
2023-06-06 22:23:42 -0300

    Finalizado 06/06

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package br.com.belval.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.belval.crud.model.Produto;
 import br.com.belval.crud.repository.ProdutoRepository;
-import ch.qos.logback.core.model.Model;
+
 
 @Controller
 public class ProdutoController {
